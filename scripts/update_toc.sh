589790bf2ab96daf26055c04a8dfa366ba623816
maxlath <k@maxlath.eu>
2020-01-10 17:25:44 +0100

    update_toc: make installing doctoc optional

--- scripts/update_toc.sh
+++ scripts/update_toc.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env bash
-doctoc README.md
-# Override summuries to directly link to the right doc
-sed -i 's/#setup/SETUP.md/' ./README.md
+which doctoc && {
+  doctoc README.md
+  # Override summuries to directly link to the right doc
+  sed -i 's/#setup/SETUP.md/' ./README.md
+} || {
+  echo "requires to have https://www.npmjs.com/package/doctoc installed, either globally or just in this repo"
+  echo "(it is not installed as a dev dependency as the use made of it it's not worth the subdependencies maintainance)"
+  exit 1
+}
