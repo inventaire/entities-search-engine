b6668632f7c2db95895f02231bcf3d517343fbb7
maxlath <k@maxlath.eu>
2020-05-12 10:48:49 +0200

    scripts: update_toc: refactor

--- scripts/update_toc.sh
+++ scripts/update_toc.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
-which doctoc && {
-  doctoc README.md
-  # Override summuries to directly link to the right doc
-  sed -i 's/#setup/SETUP.md/' ./README.md
-} || {
+#!/usr/bin/env sh
+which doctoc > /dev/null || {
   echo "requires to have https://www.npmjs.com/package/doctoc installed, either globally or just in this repo"
-  echo "(it is not installed as a dev dependency as the use made of it it's not worth the subdependencies maintainance)"
+  echo "(it is not installed as a dev dependency as the use made of it is not worth the subdependencies maintainance)"
   exit 1
 }
+
+doctoc README.md
+# Override summuries to directly link to the right doc
+sed -i 's/#setup/SETUP.md/' ./README.md
